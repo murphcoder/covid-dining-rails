6e385fb20df5554e79d6fda149376b9987b8a929
murphcoder <murphcoder@gmail.com>
2020-11-14 05:17:01 -0600

    final error updates

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,13 +1,17 @@
 class SessionsController < ApplicationController
 
     def new
+        @user = User.new
     end
 
     def create
-        user = User.find_by(user_name: params[:user_name])
-        if user && user.authenticate(params[:password])
+        @user = User.find_by(user_name: params[:user_name])
+        if @user && @user.facebook
+            @user.errors[:facebook] << "users must use Facebook login"
+            render :new
+        elsif @user && @user.authenticate(params[:password])
             session[:user_id] = user.id
-            redirect_to user_path(user)
+            redirect_to user_path(@user)
         else
             flash[:notice] = "Invalid log in. Try again."
             redirect_to signin_path
--- app/models/user.rb
+++ app/models/user.rb
@@ -4,12 +4,19 @@ class User < ApplicationRecord
     has_many :restaurants, through: :reviews
     validates_presence_of :user_name, :email, :password, unless: :facebook
     validates_uniqueness_of :user_name, :email, unless: :facebook
+    validate :email_format
 
     def checked
         if self.admin
             "checked"
         else
             "unchecked"
+        end
+    end
+
+    def email_format
+        if self.email !~ /.*[@].*[.].*/ && !self.email.empty?
+            self.errors[:email] << "address is improperly formatted"
         end
     end
 
